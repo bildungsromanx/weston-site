cde79969387944e206148a8910826fa159b09b81
Eileen Zelaya <zelayaeileen@outlook.com>
2026-01-04 15:56:46 -0500

    again

--- .next/dev/server/chunks/ssr/[root-of-the-server]__fc2e877f._.js
+++ .next/dev/server/chunks/ssr/[root-of-the-server]__fc2e877f._.js
@@ -66,7 +66,7 @@ function HomePage() {
         children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
             className: "w-full",
             children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("main", {
-                className: "w-full min-h-[105vh] pt-0 pb-20 bg-cover md:bg-cover bg-top md:bg-center transition-colors duration-300 -mt-30 opacity-90",
+                className: "w-full min-h-[105vh] pt-0 pb-20 bg-cover md:bg-cover bg-top md:bg-center transition-colors duration-300 -mt-30 opacity-95",
                 style: {
                     backgroundImage: "url('/drawing/14.webp')"
                 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function HomePage() {
     <PageWithNavbar variant="dark">
       <div className="w-full">
         <main
-          className="w-full min-h-[105vh] pt-0 pb-20 bg-cover md:bg-cover bg-top md:bg-center transition-colors duration-300 -mt-30 opacity-90"
+          className="w-full min-h-[105vh] pt-0 pb-20 bg-cover md:bg-cover bg-top md:bg-center transition-colors duration-300 -mt-30 opacity-95"
           style={{
             backgroundImage: "url('/drawing/14.webp')",
           }}
